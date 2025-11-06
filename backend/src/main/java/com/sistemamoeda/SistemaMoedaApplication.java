f608f707c65dfd3061028302e779e0aa48bf5cf3
Arthur Pedra <118223096+arthies2323@users.noreply.github.com>
2025-11-06 15:48:18 -0300

    Adicionando a função de email

--- backend/src/main/java/com/sistemamoeda/SistemaMoedaApplication.java
+++ backend/src/main/java/com/sistemamoeda/SistemaMoedaApplication.java
@@ -3,6 +3,8 @@ package com.sistemamoeda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class SistemaMoedaApplication {
     public static void main(String[] args) {
